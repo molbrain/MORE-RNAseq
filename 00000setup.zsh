b93363f35cc0fad9f27e98363347bf03d5fd4a8d
Dept. Molecular Brain Science, Kumamoto Univ <104294742+molbrain@users.noreply.github.com>
2023-07-20 01:00:03 +0900

    Add files via upload

--- 00000setup.zsh
+++ 00000setup.zsh
@@ -4,9 +4,8 @@
 ## 00000setup.zsh
 #################################
 
-echo -n "## source $0 START : "
+echo -n "## source $0 start : "
 date
-
 
 
 ### NEED TO CONFIRM/MODIFIY #####
@@ -15,6 +14,7 @@ date
 # SPECIES: human -> =homo_sapiens, mouse -> mus_musculus
 GENOME=GRCm38
 SPECIES=mus_musculus
+
 #CAP_SPECIES=Mus_musculus (or the below Perl oneliner do replace the first character to capital automatically)
 #GENOME=
 #SPECIES=
@@ -23,7 +23,6 @@ CAP_SPECIES=`echo -n $SPECIES | perl -pe 's/^(.)/\U$1/;'`
 # ENSRELEASE is the release number of Ensembl as the FASTA/GTC sources
 # Example workflow, used the data of release 102 as the original references
 ENSRELEASE=102
-
 
 # READ_LENGTH (read length for Cutadapt)
 # For example, "151" length frequently includes the additional one base for quality guarantee.
@@ -36,14 +35,31 @@ MIN_LENGTH=40
 # MIN_QUAL (minimum base quality for STAR)
 MIN_QUAL=10
 
-# PAIRREAD (Is the RNA-seq data pair-end reads, or single-end reads? For STAR/RSEM)
-PAIRREAD=PE
+# NEXTSEQ_TRIM (nextseq-trim option for Cutadapt)
+NEXTSEQ_TRIM=5
 
-# THREAD (If available, use the number of thread setting)
-THREAD=3
+# ISPAIREDREAD (Is the RNA-seq data pair-end reads, or single-end reads? For STAR/RSEM)
+# ISPAIREDREAD=1: single-end  ---> will input fastq as single-end in STAR, not paired-end
+# ISPAIREDREAD=2: paired-end  --> will use --paired-end option in RSEM, not as single-end
+ISPAIREDREAD=2
+
+# STRANDEDNESS (strandedness for RSEM) 
+# --strandedness <none|forward|reverse>
+# For Illumina TruSeq Stranded protocols, should use 'reverse'. (RSEM default is none)
+STRANDEDNESS=none
+
+# The number of threads
+THREAD=4
+
+# If you need, you can modified these directory name
+RAWDATA_DIR=Rawdata
+RESULT_DIR=Results
 
 ### PATH (NEED TO MODIFY) ######
 
+#JAVA=/bin/java
+#PERL=/usr/bin/perl
+#R=/usr/local/R/bin/R
 JAVA=java
 PERL=perl
 R=R
@@ -51,6 +67,9 @@ R=R
 TOOL_FASTQC=fastqc
 TOOL_FASTQSTATS=fastq-stats
 TOOL_CUTADAPT=cutadapt
+TOOL_TRIMMOMATIC=trimmomatic
+# If you don't use an executable Trimmomatic but a jar file, please set the below
+#JAR_TRIMMOMATIC=Trimmomatic.jar
 JAR_TRIMMOMATIC=/usr/local/Trimmomatic-0.38/trimmomatic-0.38.jar
 TOOL_STAR=STAR
 TOOL_RSEM_PREP_REF=rsem-prepare-reference
@@ -60,20 +79,26 @@ TOOL_SAMTOOLS=samtools
 
 
 
+
+
 #### NO NEED TO MODIFY THE BELOW LINES ##########
 
-# Reference name
-L1_GTF=./MORE/MORE-reference/L1fli_Utr5toUtr3.${GENOME}.gtf
+# References and directories
+L1_GTF=./MORE-RNAseq/MORE-reference/L1fli_Utr5toUtr3.${GENOME}.gtf
 REF_NAME=${GENOME}.${ENSRELEASE}
-
-
-# Directories
-
-RAWDATA_DIR=Rawdata
-RESULT_DIR=Results
 
 STAR_RSEM_REF_DIR=Reference
 ORIGINAL_REF_DIR=${STAR_RSEM_REF_DIR}/Original
+
+REF_FA=${STAR_RSEM_REF_DIR}/${REF_NAME}.fasta
+REF_GTF=${STAR_RSEM_REF_DIR}/${REF_NAME}.gtf
+
+ORIGINAL_REF_FA=${ORIGINAL_REF_DIR}/${CAP_SPECIES}.${GENOME}.dna.primary_assembly.fa.gz
+ORIGINAL_REF_GTF=${ORIGINAL_REF_DIR}/${CAP_SPECIES}.${GENOME}.${ENSRELEASE}.gtf.gz
+
+ORIGINAL_REF_FTPSITE=ftp.ensembl.org/pub/release-${ENSRELEASE}
+ORIGINAL_REF_FA_URL=${ORIGINAL_REF_FTPSITE}/fasta/${SPECIES}/dna/${ORIGINAL_REF_FA}
+ORIGINAL_REF_GTF_URL=${ORIGINAL_REF_FTPSITE}/gtf/${SPECIES}/${ORIGINAL_REF_GTF}
 
 TEMP_DIR=TEMP
 OUTPUT_FASTQC_DIR=${TEMP_DIR}/FASTQC
@@ -84,12 +109,26 @@ OUTPUT_STAR_DIR=${TEMP_DIR}/STAR
 OUTPUT_RSEM_DIR=${TEMP_DIR}/RSEM
 OUTPUT_EDGER_DIR=${TEMP_DIR}/EDGER
 
-
 # Confrim tools and those versions
+if [ -e `which ${JAVA}` ]
+then
+    echo -n "## java :" `which ${JAVA}`
+    ${JAVA} -version
+else
+    echo "## NO exist : ${JAVA}"
+fi
+
+if [ -e `which ${PERL}` ]
+then
+    echo "## Perl : " `which ${PERL}`
+    ${PERL} -version 2>&1 | grep version
+else
+    echo "## NO exist : ${PERL}"
+fi
 
 if [ -e `which ${TOOL_FASTQC}` ]
 then
-    echo "## FastQC is exist:" `which ${TOOL_FASTQC}`
+    echo "## FastQC :" `which ${TOOL_FASTQC}`
     ${TOOL_FASTQC} -version
 else
     echo "## NO exist : ${TOOL_FASTQC}"
@@ -97,78 +136,68 @@ fi
 
 if [ -e `which ${TOOL_FASTQSTATS}` ]
 then
-    echo "## fastq-stats is exist:" `which ${TOOL_FASTQSTATS}`
+    echo "## fastq-stats :" `which ${TOOL_FASTQSTATS}`
     ${TOOL_FASTQSTATS} -help 2>&1 | grep Version
 else
     echo "## NO exist : ${TOOL_FASTQSTATS}"
 fi
 
-if [ -e `which ${JAVA}` ]
+PATH_TRI=`which ${TOOL_TRIMMOMATIC}`
+if [ -e $PATH_TRI ]
 then
-    echo "## java is exist:" `which ${JAVA}`
-    ${JAVA} -version
-else
-    echo "## NO exist : ${JAVA}"
-fi
-
-if [ -e ${JAR_TRIMMOMATIC} ]
+    echo "## Trimmomatic : " `echo ${TOOL_TRIMMOMATIC}`
+    ${TOOL_TRIMMOMATIC} -version
+elif [ -f ${JAR_TRIMMOMATIC} ]
+    echo "## Trimmomatic : ${JAR_TRIMMOMATIC}" 
+    ${JAVA} -jar ${JAR_TRIMMOMATIC} -version
 then
-    echo "## JAR_TRIMMOMATIC is exist:" ${JAR_TRIMMOMATIC}
-    echo "Trimmomatic" `${JAVA} -jar ${JAR_TRIMMOMATIC} -version`
 else
-    echo "## NO exist : ${JAR_TRIMMOMATIC}"
+    echo "## NO exist : ${JAR_TRIMMOMATIC} ${TOOL_TRIMMOMATIC}"
 fi
 
 if [ -e `which ${TOOL_STAR}` ]
 then
-    echo "## STAR is exist:" `which ${TOOL_STAR}`
-    echo "STAR" `${TOOL_STAR} --version`
+    echo "## STAR :" `which ${TOOL_STAR}`
+    ${TOOL_STAR} --version
 else
     echo "## NO exist : ${TOOL_STAR}"
 fi
 
-if [ -e `which ${PERL}` ]
-then
-    echo "## Perl is exist:" `which ${PERL}`
-    ${PERL} -version 2>&1 | grep version
-else
-    echo "## NO exist : ${PERL}"
-fi
-
 if [ -e `which ${TOOL_RSEM_PREP_REF}` ]
 then
-    echo "RSEM is exist:" `which ${TOOL_RSEM_PREP_REF}`
+    echo "## RSEM (rsem-prepare-reference) :" `which ${TOOL_RSEM_PREP_REF}`
 else
     echo "## NO exist : ${TOOL_RSEM_PREP_REF}"
 fi
 
 if [ -e `which ${TOOL_RSEM_CALC_EXPR}` ]
 then
-    echo "RSEM is exist:" `which ${TOOL_RSEM_CALC_EXPR}`
+    echo "## RSEM (rsem-calculate-expression) : " `which ${TOOL_RSEM_CALC_EXPR}`
 else
-    echo "## NO exist : ${TOOL_RSEM_CALC_EXPR} `which ${TOOL_RSEM_CALC_EXPR}`"
+    echo "## NO exist : ${TOOL_RSEM_CALC_EXPR}"
 fi
 
 if [ -e `which ${TOOL_RSEM_GEN_DATA_MTX}` ]
 then
-    echo "RSEM is exist:" `which ${TOOL_RSEM_GEN_DATA_MTX}`
+    echo "## RSEM (rsem-generate-data-matrix) : " `which ${TOOL_RSEM_GEN_DATA_MTX}`
 else
     echo "## NO exist : ${TOOL_RSEM_GEN_DATA_MTX}"
 fi
 
 if [ -e `which ${TOOL_SAMTOOLS}` ]
 then
-    echo "samtools is exist:" `which ${TOOL_SAMTOOLS}`
-    echo "samtools" `${TOOL_SAMTOOLS} --version`
+    echo "## samtools : " `which ${TOOL_SAMTOOLS}`
+    ${TOOL_SAMTOOLS} --version
 else
     echo "## NO exist : ${TOOL_SAMTOOLS}"
 fi
 
 if [ -e `which ${R}` ]
 then
-    echo "R is exist:" `which ${R}`
+    echo -n "## R :" `which ${R}` " "
     ${R} --version | grep "R version"
-    ${R} --slave < <(echo "library(edgeR);sessionInfo();") 2>&1 | grep edgeR | perl -e 'while(<>){chomp;$l .=$_;} if($l=~/edgeR/){print "$l\n";}else{print "## NOT installed : edgeR\n";}'
+    echo -n "## edgeR :"
+    ${R} --slave < <(echo "library(edgeR);sessionInfo();") 2>&1 | grep edgeR
 else
     echo "## NO exist : ${R} (and no edgeR, too)"
 fi
@@ -176,5 +205,5 @@ fi
 
 #############################
 
-echo -n "## source $0 DONE : "
+echo -n "## source $0 finished : "
 date
