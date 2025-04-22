431f5b0e6e6457fe38d2105273ee623a5fcb5d16
Dept. Molecular Brain Science, Kumamoto Univ. <104294742+molbrain@users.noreply.github.com>
2025-04-22 18:32:38 +0900

    Update docker.sh
    Update for the new version of Dockerfile and the $TESTDATA directory branch.

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,24 +1,20 @@
 #!/bin/sh
 
-## your data directory name
-#TESTDATA=230809_Docker_forMORE-RNAseq
-
-
+## Please change 'testdata' to your data directory name
+TESTDATA=testdata
 
 docker build -t centos7:MORE-RNAseq .
-
-
 
 docker run -dit --name more-rnaseq centos7:MORE-RNAseq
 
 
+if [ -d ./${TESTDATA} ]
+then
+   echo "${TESTDATA} dirctory is copying to docker image as '/root/testdata/' ..."
+   docker cp ./${TESTDATA}/ more-rnaseq:/root/testdata/
+else
+   echo "If you want to copy your data to docker-image, do the command like this: 'docker cp ./your-data-directory/ more-rnaseq:/root/testdata/'."
+fi
+echo "If you want to copy your data from docker image, do the command like this: 'docker cp more-rnaseq:/root/testdata/Results/ ./CopiedResults/'."
 
-#docker cp ./${TESTDATA}/ more-rnaseq:/root/testdata/
-
-
-
-docker exec -it more-rnaseq /bin/bash
-
-
-
-# docker cp more-rnaseq:/root/testdata/Results/ ./CopiedResults/
+docker exec -it more-rnaseq /bin/zsh
